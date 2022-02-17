376cbbb6521022aab76577f641ea159ca7af9058
Pavel Anikeev <delemy@yandex.ru>
2022-02-17 20:47:18 +0300

    requestAnimationFrame  unit

--- unit5 setTimeOut Clearinterval/script.js
+++ unit5 setTimeOut Clearinterval/script.js
@@ -1,19 +1,32 @@
-const btn = document.querySelector(".btn");
+const btn = document.querySelector(".btn"),
+  elem = document.querySelector(".box");
+let pos = 0;
+
+//! old ver animation
+// function myAnimation() {
+
+//   let pos = 0;
+
+//   const id = setInterval(frame, 10);
+//   function frame() {
+//     if (pos == 300) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       elem.style.top = pos + "px";
+//       elem.style.left = pos + "px";
+//     }
+//   }
+// }
 
 function myAnimation() {
-  const elem = document.querySelector(".box");
-  let pos = 0;
+  pos++;
+  elem.style.top = pos + "px";
+  elem.style.left = pos + "px";
 
-  const id = setInterval(frame, 10);
-  function frame() {
-    if (pos == 300) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.top = pos + "px";
-      elem.style.left = pos + "px";
-    }
+  if (pos < 300) {
+    requestAnimationFrame(myAnimation);
   }
 }
 
-btn.addEventListener("click", myAnimation);
+btn.addEventListener("click", () => requestAnimationFrame(myAnimation));
